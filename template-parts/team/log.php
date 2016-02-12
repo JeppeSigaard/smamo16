df52768635dc141f1e46d91b8245fa88e5666f82
JeppeSigaard <jeppe@smatmonkey.dk>
2016-02-12 14:31:26 +0100

    log layout

--- template-parts/team/log.php
+++ template-parts/team/log.php
@@ -1,26 +1,31 @@
 <?php 
 
+$author_title = get_the_title();
 $log = new WP_Query(array(
-    'post_type' => 'log',
+    'post_type' => 'logbog',
     'author' => get_the_author_meta('ID'),
 ));
 
-
 if ($log->have_posts()):
-
 ?>
 
 
 <div class="team-log">
     <div class="inner">
-        <?php while ($log->have_posts()) : $log->the_post(); ?>
+        <header class="log-header">
+            <h3><?php echo $author_title ?>'s log</h3>
+        </header>
+        <?php while ($log->have_posts()) : $log->the_post(); $date = get_the_date('Y-m-d'); $current_date = date('Y-m-d', time()); ?>
+        <?php if($date !== $current_date) the_date('d. F Y  ','<div class="log-entry-date">','</div>'); ?>
         <div class="log-entry">
-            <div class="log-entry-meta">
-                <span><?php the_date(); ?></span>
-            </div>
-            <div class="log-entry-content">
-                <?php the_content(); ?>
-            </div>
+            <span class="log-entry-time">
+            <?php 
+                
+                if ( $date === $current_date ){printf( _x( '%s siden', '%s = human-readable time difference', 'smamo' ), human_time_diff( get_the_time( 'U' ), current_time( 'timestamp' ) ) ); }
+                else{the_time('H.i');}
+            ?>
+            </span>
+            <?php the_content(); ?>
         </div>
         <?php endwhile; wp_reset_postdata(); ?>
     </div>
