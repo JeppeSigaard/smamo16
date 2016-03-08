63fe06fa0ea029543103a91e49031c098f75b72f
JeppeSigaard <jeppe@smatmonkey.dk>
2016-03-08 16:33:08 +0100

    minder opdateringer

--- js/components/data-img.js
+++ js/components/data-img.js
@@ -1,51 +1,75 @@
-// Load images into placeholders
-// Use data-bg="" on any element to async load an image background
-// Use data-src="" on images to async load an image src
+var jio_data_img = {
+  
+  // $img is in view (true | false)
+  inview : function($img){
+    var w = $(window);
+    if(w.scrollTop() + w.height() <= $img.offset().top){ return false; }
+    else if (w.scrollTop() >= $img.offset().top + $img.height()){ return false; }
+    else { return true; }
+  },
+  
+  // placeholder load $img
+  replace: function($img) {
+    var $imgSrc = $img.attr('data-src'),
+      $imgBg = $img.attr('data-bg'),
+      $imgSrcset = $img.attr('data-srcset'),
+      $imgSizes = $img.attr('data-sizes'),
+      $imgPlaceholder = new Image();
 
+    if (typeof $imgSrc !== 'undefined') {
+      $imgPlaceholder.src = $imgSrc;
+      $imgPlaceholder.onload = function() {
+        $img.removeClass('loading')
+          .removeAttr('data-src')
+          .attr('src', $imgSrc);
+      };
+    };
 
-var loadDataImages = function(){
-    
-    // For each background placeholder
-    $('[data-bg]').each(function(e){
-        
-            // grab 'this', the data-bg attribute and make a new placeholder image object
-            var $img = $(this),
-                $imgSrc = $(this).attr('data-bg'),
-                $imgPlaceholder = new Image();
-            
-            // render background into local storage by applying it to the placeholder
-            $imgPlaceholder.src = $imgSrc;
-            
-            // When the placeholder is loaded
-            $imgPlaceholder.onload = function(){
-                
-                // Apply the source to 'this', hide 'this' and fade it in
-                $img.removeAttr('data-bg').removeClass('loading').css({
-                    backgroundImage: 'url('+$imgSrc+')',
-                });
-            };
+    if (typeof $imgSrcset !== 'undefined') {
+      $imgPlaceholder.srcset = $imgSrcset;
+      $imgPlaceholder.sizes = $imgSizes;
+      $imgPlaceholder.onload = function() {
+        $img.removeClass('loading')
+          .removeAttr('data-srcset')
+          .removeAttr('data-sizes')
+          .attr('srcset', $imgSrcset)
+          .attr('sizes', $imgSizes);
+      };
+    }
+
+    if (typeof $imgBg !== 'undefined') {
+      $imgPlaceholder.src = $imgBg;
+      $imgPlaceholder.onload = function() {
+        $img.removeClass('loading')
+          .removeAttr('data-bg')
+          .css({
+          backgroundImage: 'url(' + $imgBg + ')',
         });
-        
-    
-        // Basically same thing with images
-        
-        
-        $('img[data-src]').each(function(e){
-            
-            var $img = $(this),
-                $imgSrc = $(this).attr('data-src'),
-                $imgPlaceholder = new Image();
-            
-            $imgPlaceholder.src = $imgSrc;
-            $imgPlaceholder.onload = function(){
-                $img.removeAttr('data-src').removeClass('loading').attr('src',$imgSrc);
-            };
+      };
+    };
+  },
+  
+  // scan DOM for replacable img srcses
+  scan : function(){
+    $('[data-src], [data-bg], [data-srcset], [data-sizes]').each(function(e){
+      var $img = $(this);  
+      if(jio_data_img.inview($img)){
+        jio_data_img.replace($img);
+      }
+    });
+  },
+  
+  // Initiate on each event in array
+  init : function(events){
+    if ($.isArray(events)){
+      events.forEach(function(event){
+        $(window).on(event,function(){
+          jio_data_img.scan();
         });
-
-
+      });
+    }
+  }
 }
 
-// Run on load (and use in other scripts as needed)
-$(window).on('load',function(){
-    loadDataImages();
-});
+// Lesgo!
+jio_data_img.init(['load','scroll','resize']);
