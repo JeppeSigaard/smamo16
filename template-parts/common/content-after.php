3cce3421ee3ff90cc7868ede6e543307d78db09f
JeppeSigaard <jeppe@smatmonkey.dk>
2016-03-09 12:17:42 +0100

    ..

--- template-parts/common/content-after.php
+++ template-parts/common/content-after.php
@@ -13,10 +13,10 @@ elseif ('team' === get_post_type(get_the_ID())) {
 
 else{    
     get_template_part('template-parts/common/contact-spinner');
-
+    get_template_part('template-parts/common/case-links'); 
 }
 
-get_template_part('template-parts/common/case-links'); 
+
 
 ?>
 </div>
